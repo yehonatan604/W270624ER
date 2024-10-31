32998b94e7ab8133a9a2f2df36fa49055ce21f0b
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-10-31 18:20:35 +0200

    variabales

---
+++ JS/31-10-2024/script.js
@@ -0,0 +1,44 @@
+var myNumber1 = "5.2";
+var myNumber2 = "20.2";
+
+// Casting - המרה
+// method 1
+console.log(+myNumber1 + +myNumber2);
+
+// method 2
+console.log(parseInt(myNumber1) + parseInt(myNumber2));
+
+// method 3
+console.log(parseFloat(myNumber1) + parseFloat(myNumber2));
+
+// method 4
+console.log(Number(myNumber1) + Number(myNumber2));
+
+// ****************** prompt *******************
+
+// var userNumber1 = prompt("Please enter a number1:");
+// var userNumber2 = prompt("Please enter a number2:");
+// console.log(+userNumber1 + +userNumber2);
+
+// ****************** let *******************
+console.log("****************** let *******************");
+
+
+var num1 = 5;
+var num1 = 10;
+
+console.log(num1);
+
+let num2 = 5;
+num2 = 10;
+
+console.log(num2);
+
+// ****************** const *******************
+console.log("****************** const *******************");
+
+const num3 = 5;
+//num3 = 10;
+
+
+const myBool = true;
