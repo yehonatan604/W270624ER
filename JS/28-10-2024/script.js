2a3647b5f1f20a3047c91c9720588f221561acb2
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-10-31 16:57:31 +0200

    28-10-2024

---
+++ JS/28-10-2024/script.js
@@ -0,0 +1,137 @@
+/*
+    This is a multi-line comment
+    This is a multi-line comment
+*/
+
+// this console.log will be executed when the page is loaded
+console.log("Hello World!");
+console.log('Hello World!');
+console.log(`Hello World!`);
+
+// string: "Hello World!"
+console.log('Hello my name is "John"');
+
+//number: 6
+
+console.log(6 + 6); // 12
+console.log("6" + "6"); //"66"
+console.log("6" + 6 + 6); // "666"
+
+// + - * / 
+
+console.log("*************************************");
+
+var myNumber = 50;
+
+console.log(myNumber + 50); // 100
+console.log(myNumber); // 50
+
+myNumber = myNumber + 50;
+console.log(myNumber); // 100
+
+myNumber += 50;
+
+// boolean: true, false
+console.log(myNumber == 50); // false
+console.log(myNumber == 150); // true
+
+console.log(150 == "150"); // true
+console.log(150 === "150"); // false
+
+console.log("*************************************");
+console.log(true == 1); // true
+console.log(true === 1); // false
+
+console.log(9 % 2); // 1 (9 / 2 = 4 and the remainder is 1)
+console.log(7 / 4); // 1.75
+console.log(7 % 4); // 3 (7 / 4 = 1 and the remainder is 3)
+
+console.log(4 % 2 == 0); // true
+
+console.log("*************************************");
+
+var myNumber2 = 10;
+// method 1
+myNumber2 = myNumber2 + 1;
+
+// method 2
+myNumber2 += 1;
+
+// method 3
+myNumber2++;
+
+console.log("is 13?");
+console.log(myNumber2);
+
+
+console.log("*************************************");
+console.log(3 * 3 === 9);
+console.log(3 * 3 !== 9);
+
+
+console.log("*************************************");
+console.log(3 > 2); // true
+console.log(3 < 2); // false
+
+console.log(3 >= 3); // true
+
+console.log("*************************************");
+
+var isOk = 10;
+console.log(isOk == "10");
+console.log(isOk != "10");
+
+//console.log(0.1 + 0.2)
+
+console.log("*************************************");
+
+console.log(3 > 2 && 10 > 4); // true
+console.log(3 > 2 && 10 > 58); // false
+
+console.log(3 > 2 || 10 > 58); // true
+console.log(3 < 2 || 10 < 4); // false
+
+console.log("*************************************");
+console.log("***********variables**************************");
+console.log("*************************************");
+
+var someCalculation;
+
+someCalculation = 10 + 10;
+
+var x, y, z;
+
+x = 6;
+y = 7.7;
+z = 8;
+
+console.log(x, y, z);
+
+
+var myName = "John";
+var fullNumber = 10;
+var floatingNumber = 10.5;
+console.log(typeof myName);
+console.log(typeof fullNumber);
+console.log(typeof floatingNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
