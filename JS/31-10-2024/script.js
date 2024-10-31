f06b1a909ead03ed5e604982f63d8b87e55b9876
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-10-31 19:39:24 +0200

    functions

--- JS/31-10-2024/script.js
+++ JS/31-10-2024/script.js
@@ -42,3 +42,59 @@ const num3 = 5;
 
 
 const myBool = true;
+
+// ****************** functions *******************
+console.log("****************** functions *******************");
+
+
+// function declaration
+function shahar() {
+    // function scope
+    const userNumber1 = +prompt("Please enter a number1:");
+    const userNumber2 = +prompt("Please enter a number2:");
+    const sum1 = userNumber1 + userNumber2;
+    console.log(userNumber1 + " + " + userNumber2 + " = " + sum1);
+}
+
+console.log(shahar);
+console.log(typeof shahar);
+
+// shahar();
+// shahar();
+// shahar();
+
+// console.log(userNumber1); - error - out of scope
+
+// ****************** parameters *******************
+console.log("****************** parameters *******************");
+
+function addNumbers(x, y) {
+    const sum2 = x + y;
+    console.log(x + " + " + y + " = " + sum2);
+}
+
+addNumbers();
+addNumbers(100, 200);
+addNumbers(50, 30);
+addNumbers(30, 24);
+
+// ****************** return *******************
+
+console.log("****************** return *******************");
+
+function calcSquareArea(length, width) {
+    const area = length * width;
+    return area;
+}
+
+calcSquareArea(5, 5);
+
+const square1 = calcSquareArea(5, 5);
+console.log(square1);
+
+
+const square2 = calcSquareArea(7, 4);
+console.log(square2 * square1);
+console.log(square2 + square1);
+console.log(square2 - square1);
+console.log(square2 * square1);
