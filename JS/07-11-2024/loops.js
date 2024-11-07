783e45bd5a8834279d8d623b0ffdbf8434358f3f
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-11-07 20:06:34 +0200

    while-loops

---
+++ JS/07-11-2024/loops.js
@@ -0,0 +1,15 @@
+let myNumber = 0;
+
+while (myNumber < 100) {
+    myNumber++;
+    console.log(myNumber);
+}
+
+console.log("***************************");
+
+let myNumber2 = 5;
+
+while (myNumber2 <= 10) {
+    console.log(myNumber2);
+    myNumber2++;
+}
