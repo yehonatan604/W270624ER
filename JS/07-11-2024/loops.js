cd8be800265e831ce7788b37dcf4914c555e3c68
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-11-07 20:50:44 +0200

    loops

--- JS/07-11-2024/loops.js
+++ JS/07-11-2024/loops.js
@@ -13,3 +13,28 @@ while (myNumber2 <= 10) {
     console.log(myNumber2);
     myNumber2++;
 }
+
+console.log("**************q5*************");
+
+let myNumber3 = 2;
+
+while (myNumber3 <= 20) {
+    console.log(myNumber3);
+    myNumber3 += 2;
+}
+
+console.log("**************for-loops*************");
+
+// for loops syntax:
+// for (decleratiom; condition; increment/decrement) {...}
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+
+
+const bla = prompt("Please enter a number:");
+
+for (let i = bla; i <= 100; i++) {
+    console.log(i);
+}
