280125968bd42a916365d55cb80387abbdfe5536
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-11-11 19:02:44 +0200

    arrays

--- JS/07-11-2024/loops.js
+++ JS/07-11-2024/loops.js
@@ -1,40 +1,94 @@
-let myNumber = 0;
+// let myNumber = 0;
 
-while (myNumber < 100) {
-    myNumber++;
-    console.log(myNumber);
-}
+// while (myNumber < 100) {
+//     myNumber++;
+//     console.log(myNumber);
+// }
 
-console.log("***************************");
+// console.log("***************************");
 
-let myNumber2 = 5;
+// let myNumber2 = 5;
 
-while (myNumber2 <= 10) {
-    console.log(myNumber2);
-    myNumber2++;
-}
+// while (myNumber2 <= 10) {
+//     console.log(myNumber2);
+//     myNumber2++;
+// }
 
-console.log("**************q5*************");
+// console.log("**************q5*************");
 
-let myNumber3 = 2;
+// let myNumber3 = 2;
 
-while (myNumber3 <= 20) {
-    console.log(myNumber3);
-    myNumber3 += 2;
-}
+// while (myNumber3 <= 20) {
+//     console.log(myNumber3);
+//     myNumber3 += 2;
+// }
 
-console.log("**************for-loops*************");
+// console.log("**************for-loops*************");
 
 // for loops syntax:
 // for (decleratiom; condition; increment/decrement) {...}
 
-for (let i = 1; i <= 100; i++) {
-    console.log(i);
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+
+// const start = +prompt("Please enter start number:");
+// const end = +prompt("Please enter end number:");
+
+// for (let i = start; i <= end; i++) {
+//     console.log(i);
+// }
+
+
+console.log("**************q9*************");
+
+const start = +prompt("Please enter start number:");
+const end = +prompt("Please enter end number:");
+
+if (start > end) {
+    for (let i = start; i >= end; i--) {
+        console.log(i);
+    }
+} else {
+    for (let i = start; i <= end; i++) {
+        console.log(i);
+    }
 }
 
+console.log("**************q11*************");
 
-const bla = prompt("Please enter a number:");
+const numberOfWords = +prompt("Please enter number of words:");
+let sentence = "";
 
-for (let i = bla; i <= 100; i++) {
-    console.log(i);
+for (let i = 1; i <= numberOfWords; i++) {
+    const word = prompt("Please enter word number " + i);
+    sentence += word + " ";
 }
+
+console.log(sentence);
+
+console.log("**************multiply*************");
+
+let board = "";
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        board += i * j + "\t";
+    }
+    board += "\n";
+}
+
+console.log(board);
+
+// console.log("**************multiply board*************");
+
+// let board = ""
+// for (let i = 1; i <= 10; i++) {
+//     for (let j = 1; j <= 10; j++) {
+//         board += i * j + "\t";
+//     }
+//     board += "\n";
+// }
+
+// console.log(board);
---
+++ JS/11-11-2024/script.js
@@ -0,0 +1,23 @@
+const fruites = ["Lemon", "Apple", "Banana", "Mango"];
+console.log(fruites);
+
+fruites[0] = "Orange";
+
+console.log(fruites);
+console.log(fruites.length); // length is the property of array which returns the number of elements in the array
+console.log(typeof fruites);
+
+fruites.push("Grapes"); // push() method is used to add an element at the end of the array
+console.log(fruites);
+
+fruites[5] = "Pineapple";
+console.log(fruites);
+
+console.log(fruites[fruites.length - 1]);
+console.log(fruites[fruites[0]]);
+
+
+
+
+
+
