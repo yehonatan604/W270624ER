ef628b6bb7e3797b1537d4ef9144a15969a17f0d
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-11-14 19:08:08 +0200

    update loops

--- JS/14-11-2024/script.js
+++ JS/14-11-2024/script.js
@@ -6,6 +6,7 @@ const fruites = ['Apple', 'Banana', 'Mango', 'Orange', 'Peach', 'Cherry'];
 
 for (let i = 0; i < fruites.length; i++) {
     console.log((i + 1) + ".fruit is: " + fruites[i]);
+    break;
 }
 
 console.log("\n\n***********************for-of loop on array**************************\n\n");
@@ -30,4 +31,37 @@ for (let i in cars) {
 
     console.log(i + ".car is: " + cars[i]);
 }
+
+console.log("\n\n***********************break-continue**************************\n\n");
+
+const numbers = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+
+for (let i in numbers) {
+    // if (i == 3) {
+    //     break;
+    // }
+
+    if (numbers[i] === 500 || numbers[i] === 600) {
+        continue;
+    }
+
+    console.log("Number is: " + numbers[i]);
+}
+
+console.log("\n\n***********************split - join**************************\n\n");
+
+const text = "I am a web developer"; // ["I", "am", "a", "web", "developer"]
+
+const words = text.split(" ");
+console.log(words);
+
+const bmw = "bmw";
+const bmwArray = bmw.split("");
+bmwArray[0] = bmwArray[0].toUpperCase();
+console.log(bmwArray);
+
+// B-m-w
+// const newText = bmwArray[0] + bmwArray[1] + bmwArray[2]; // yehonatan don't allow this
+const newText = bmwArray.join("-");
+console.log(newText);
 
