647682e2eaad1133831ed7a2abe1c8ac646532df
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-11-21 18:39:02 +0200

    loops summery

---
+++ JS/21-11-2024/scripts/index.js
@@ -0,0 +1,66 @@
+const apple = "apple";
+const banana = "banana";
+const orange = "orange";
+
+const fruites = [apple, banana, orange];
+
+fruites.length; // 3
+
+console.log(fruites[0]); // first element of the array
+
+console.log(fruites[fruites.length - 1]); // last element of the array
+
+// fruit is *** apple
+// fruit is *** orange
+// fruit is *** banana
+
+// for loop on the array
+for (let i = 0; i < fruites.length; i++) {
+    console.log("fruit is *** " + fruites[i]);
+}
+
+console.log("************");
+
+// backward for loop on the array
+for (let i = fruites.length - 1; i >= 0; i--) {
+    console.log("fruit is *** " + fruites[i]);
+}
+
+console.log("************");
+
+// reverse the array
+const reversedFruites = fruites.reverse();
+
+// for loop on the array after reversing using function
+function printFruit() {
+    for (let i = 0; i < reversedFruites.length; i++) {
+        console.log("fruit is *** " + reversedFruites[i]);
+    }
+
+    const x = 50;
+    return x; // return the value of x & break the function
+    console.log("this will not be printed");
+}
+
+console.log(printFruit()); //option 1
+
+const xxx = printFruit(); //option 2
+console.log(xxx);
+
+console.log("************");
+
+const uppercasedArray = (arr) => {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let currentItem = arr[i];
+        let word = currentItem[0].toUpperCase() + currentItem.slice(1);
+        newArr.push(word);
+    }
+    return newArr;
+};
+
+const movies = ["titanic", "avatar", "inception", "interstellar"];
+console.log(uppercasedArray(movies));
+console.log(movies);
+
+
