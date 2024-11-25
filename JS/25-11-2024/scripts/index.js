5b64f2a923d57e14e49c0a7a76492daa2e5324ae
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-11-25 18:29:19 +0200

    intervals

---
+++ JS/25-11-2024/scripts/index.js
@@ -0,0 +1,20 @@
+// let count = 1;
+
+// const sayHola = () => {
+//     //console.log(count + ' Hola');
+//     document.write("<h1> Hola </h1>");
+//     count++;
+
+//     if (count > 5) {
+//         clearInterval(intervals);
+//     }
+// }
+
+// const intervals = setInterval(sayHola, 1000);
+
+const title = document.getElementById("helloTitle");
+console.log(title.innerHTML);
+
+title.innerHTML = "Hola Mundo";
+
+console.log("******************* exercise 1 *******************");
