20885d6acc65864dcfcc2109d0e075e7bfcefe0e
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-12-09 18:23:29 +0200

    appendChild

--- JS/08-12-2024 - dom-p2/script.js
+++ JS/08-12-2024 - dom-p2/script.js
@@ -4,8 +4,18 @@ const styleElement = (element) => {
     element.style.fontSize = '20px';
     element.style.boxShadow = '3px 6px 12px black';
     element.style.textAlign = 'center';
+    element.style.borderBottom = '2px solid black';
 }
 
 const container = document.getElementById('container');
 
 styleElement(container);
+console.log(container.style.fontSize);
+
+const emptyDiv = document.createElement('div');
+
+const h2 = document.createElement('h2');
+h2.innerText = 'I am a h2!!!';
+
+emptyDiv.appendChild(h2);
+document.body.appendChild(emptyDiv);
