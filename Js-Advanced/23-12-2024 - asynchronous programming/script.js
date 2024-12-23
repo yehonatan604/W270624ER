940f3e18d3f20b02512810b95b9b088fd1d2a0f2
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-12-23 21:09:22 +0200

    asynchronous h.w

--- Js-Advanced/23-12-2024 - asynchronous programming/script.js
+++ Js-Advanced/23-12-2024 - asynchronous programming/script.js
@@ -38,28 +38,51 @@ const getData = () => {
 console.log("*************** Exercise ********************");
 
 const getCountry = (country) => {
+    // Create an XMLHttpRequest object
     const request = new XMLHttpRequest();
+
+    //print the request object
     console.log(request);
 
+    // we use try catch because request can throw an error
     try {
-        request.open('GET', `https://restcountries.com/v3.1/name/${country}`, false);
+        // Open a new connection, using the GET request on the URL - GET will get the data SYNCHRONOUSLY
+        request.open('GET', `https://restcountries.com/v3.1/name/${country}`, false); // false means synchronous
+
+        // Send request
         request.send();
 
+        // Check if the request was successful
         if (request.status === 200) {
-            return JSON.parse(request.response);
+            // Parse the data from the request and return it
+            const data = JSON.parse(request.response);
+            return data;
         } else {
+            // If the request was not successful, return an error message
             return 'Country not found';
         }
     } catch (error) {
+        // If there was an error with the request, return the error message
         console.log(error.message);
     }
 };
 
+// Print the data for a specific country
 const printCountryData = (country) => {
+    // Get the data for the country using the getCountry function
     const data = getCountry(country);
+
+    // Print the data to the console
     console.log(data);
+
+    //put country flag in image tag
+    const img = document.getElementById('loading');
+    img.src = data[0].flags.png;
 }
 
-printCountryData('france');
-printCountryData('francewerwer');
+printCountryData('france'); // print the data for France
+printCountryData('israel'); // print 404 error message
+printCountryData('japan'); // print 404 error message
+printCountryData('australia'); // print 404 error message
 fn2();
+
