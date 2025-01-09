ed3584fde6972db24eab74748b1116c52d6fb468
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-01-09 20:00:37 +0200

    oop

--- JS-OOP/09-01-2025 - welcome oop/script.js
+++ JS-OOP/09-01-2025 - welcome oop/script.js
@@ -3,6 +3,12 @@ class Box {
     height = 100;
     width = 200;
 
+    resetValues() {
+        this.height = 100;
+        this.width = 200;
+        return this;
+    }
+
     setValues(height, width) {
         this.height = height;
         this.width = width;
@@ -28,3 +34,36 @@ console.log(anotherBox);
 // change the height and width of the another box
 anotherBox.setValues(300, 400);
 anotherBox.printBox();
+anotherBox.resetValues().setValues(500, 600);
+anotherBox.printBox();
+
+// constructor
+
+class Box2 {
+    width;
+    height;
+    color;
+    bgColor;
+    text;
+
+    constructor(newWidth, newHight, newColor, newBgColor, newText) {
+        this.width = newWidth;
+        this.height = newHight;
+        this.color = newColor;
+        this.bgColor = newBgColor;
+        this.text = newText;
+
+        const div = document.createElement('div');
+        div.style.width = `${this.width}px`;
+        div.style.height = `${this.height}px`;
+        div.style.color = this.color;
+        div.style.backgroundColor = this.bgColor;
+        div.innerText = this.text;
+
+        document.body.appendChild(div);
+    }
+}
+
+const bigRedBox = new Box2(200, 200, 'white', 'red', 'Hello World');
+const smallGreenBox = new Box2(100, 100, 'white', 'green', 'Small Box');
+const newbox55 = new Box2(100, 100, 'blue', 'yellow', 'text');
