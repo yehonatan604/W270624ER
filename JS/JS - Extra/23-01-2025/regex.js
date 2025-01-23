ba53c28cdbebb3cf8eb1678de55fc766503b889e
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-01-23 21:03:31 +0200

    regex

--- JS/JS - Extra/23-01-2025/regex.js
+++ JS/JS - Extra/23-01-2025/regex.js
@@ -2,10 +2,26 @@ const username = document.getElementById('username');
 const usernameHelper = document.getElementById('username-helper');
 const email = document.getElementById('email');
 const emailHelper = document.getElementById('email-helper');
+const password = document.getElementById('password');
+const passwordHelper = document.getElementById('password-helper');
+const submitButton = document.getElementById('submit-btn');
+
+const checkFormIsValid = () => {
+    const isUsernameValid = !username.classList.contains('error');
+    const isEmailValid = !email.classList.contains('error');
+    const isPasswordValid = !password.classList.contains('error');
+
+    if (isUsernameValid && isEmailValid && isPasswordValid) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+};
+
 
 username.addEventListener('input', (e) => {
     const value = e.target.value;
-    if (value.length < 3) {
+    if (value.trim().length < 3) {
         usernameHelper.innerText = 'Username must be at least 3 characters long';
         if (!username.classList.contains('error')) {
             username.classList.add('error');
@@ -14,6 +30,7 @@ username.addEventListener('input', (e) => {
         usernameHelper.innerText = '';
         username.classList.remove('error');
     }
+    checkFormIsValid();
 });
 
 email.addEventListener('input', (e) => {
@@ -29,4 +46,29 @@ email.addEventListener('input', (e) => {
         emailHelper.innerText = '';
         email.classList.remove('error');
     }
+    checkFormIsValid();
+});
+
+// minimum 8 characters length
+// at least one uppercase letter
+// at least one lowercase letter
+// at least one digit
+// at least one special character (e.g. !@#$%^&*)
+// english characters only
+
+password.addEventListener('input', (e) => {
+    const value = e.target.value;
+    const passwordRegex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+
+    if (!passwordRegex.test(value)) {
+        passwordHelper.innerText = 'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one digit, one special character and only english characters';
+        if (!password.classList.contains('error')) {
+            password.classList.add('error');
+        }
+    } else {
+        passwordHelper.innerText = '';
+        password.classList.remove('error');
+    }
+    checkFormIsValid();
 });
