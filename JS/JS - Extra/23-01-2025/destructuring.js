b840e2503554a13d6043bb7d2b38fcd6910fb6e2
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-01-23 18:52:43 +0200

    default parameters

--- JS/JS - Extra/23-01-2025/destructuring.js
+++ JS/JS - Extra/23-01-2025/destructuring.js
@@ -31,3 +31,32 @@ const { country, fname, ...restOfPerson } = person;
 console.log(fname);
 console.log(country);
 console.log(restOfPerson);
+
+console.log("\n**************** Exercise Destructuring **************\n");
+
+const people = [
+    { name: "John", age: 25, country: "USA" },
+    { name: "Jane", age: 30, country: "UK" },
+    { name: "Bob", age: 35, country: "Canada" },
+];
+
+for (const { name, country } of people) {
+    console.log(`${name} lives in ${country}`);
+};
+
+console.log("\n**************** Nested Destructuring **************\n");
+const [john] = people;
+//const nameOfJohn = john.name;
+
+let { name: nameOfJohn } = john;
+nameOfJohn = "Peter";
+console.log(nameOfJohn);
+console.log(people);
+
+console.log("\n**************** Default Parameters **************\n");
+
+const add = (num1, num2 = 50) => {
+    console.log(num1 + num2);
+}
+
+add(5);
