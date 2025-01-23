1e8f34021c3cc15718f2b96585acd03ee8c999a7
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-01-23 18:14:52 +0200

    object destrturing

--- JS/JS - Extra/23-01-2025/destructuring.js
+++ JS/JS - Extra/23-01-2025/destructuring.js
@@ -1,4 +1,4 @@
-console.log("\n******************************\n");
+console.log("\n*************** Array Destructuring ***************\n");
 // array destructuring
 const arr = [1, 2, 3, 4, 5];
 
@@ -13,9 +13,21 @@ console.log("\n******************************\n");
 // this is the new way - destructuring
 
 const [first, second, ...rest] = arr;
-
 console.log((first + second) * 4);
-console.log(rest);
 console.log(first);
+console.log(second);
+console.log(rest);
 
-console.log("\n******************************\n");
+console.log("\n**************** Object Destructuring **************\n");
+
+const person = {
+    fname: "John",
+    age: 25,
+    country: "USA",
+    job: "Developer",
+};
+
+const { country, fname, ...restOfPerson } = person;
+console.log(fname);
+console.log(country);
+console.log(restOfPerson);
