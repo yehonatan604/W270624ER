aeb13d0b73fedc054e1faf05f49ec24c03df34c1
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-02-20 19:10:24 +0200

    oop1

---
+++ TS/20-02-2025 - ts oop/ts-oop-1/src/main.ts
@@ -0,0 +1,24 @@
+class Person {
+  public name: string;
+  public age: number;
+  private readonly id: number;
+  private static count: number = 0;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+
+    Person.count++;
+    this.id = Person.count;
+
+    this.printInfo();
+  }
+
+  public printInfo() {
+    const info: string = `Name: ${this.name}, Age: ${this.age}, ID: ${this.id}`;
+    console.log(info);
+  }
+}
+
+new Person('Ortal', 28);
+new Person('Snir', 32);
