45459391e4984da53f52c2cbacf6d22c67152cff
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-02-27 20:50:13 +0200

    getter setter

--- TS/27-02-2025 - ts final/ts final/src/main.ts
+++ TS/27-02-2025 - ts final/ts final/src/main.ts
@@ -68,3 +68,83 @@ const numbers2: Array<number> = [1, 2, 3];
 console.log(numbers2);
 // numbers2.push("werewr"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'
 
+console.log("\n**********************************Getter/Setter************************************************\n");
+
+class Rectangle {
+  private _width: number;
+  private _height: number;
+  private readonly _defaultWidth;
+  private readonly _defaultHeight;
+  //width: number; // we wont need this because we will use setter
+  //height: number; // we wont need this because we will use setter
+  //area: number; // we wont need this because we will use getter
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+    this._height = height;
+    this._width = width;
+    this._defaultHeight = height;
+    this._defaultWidth = width;
+    //this.area = width * height; // we wont need this because we will use getter
+  }
+
+  reset = () => {
+    this.width = this._defaultWidth
+    this.height = this._defaultHeight;
+    //this.area = this._width * this._height; // we wont need this because we will use getter
+  }
+
+  get width() {
+    return this._width;
+  }
+  set width(value: number) {
+    if (value <= 0) {
+      this._width = -value;
+    } else {
+      this._width = value;
+    }
+  }
+
+  get height() {
+    return this._height;
+  }
+  set height(value: number) {
+    if (value <= 0) {
+      this._height = -value;
+    } else {
+      this._height = value;
+    }
+  }
+
+  get area() {
+    return this.width * this.height;
+  }
+
+  get info() {
+    return `Rectangle: width=${this.width}, height=${this.height}, area=${this.area}`;
+  }
+}
+
+
+
+
+
+
+
+
+
+const rect = new Rectangle(10, 20);
+console.log(rect.info);
+
+rect.width = 30;
+console.log(rect.info);
+
+rect.reset();
+console.log(rect.info);
+
+rect.height = -80;
+console.log(rect.info);
+
+console.log(rect.height);
+console.log(rect.width);
