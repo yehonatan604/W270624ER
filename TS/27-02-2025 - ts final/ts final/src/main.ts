91d56aa327942908586e07a239815a7d40be3b20
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-02-27 18:21:23 +0200

    optional and default parameters

--- TS/27-02-2025 - ts final/ts final/src/main.ts
+++ TS/27-02-2025 - ts final/ts final/src/main.ts
@@ -1,7 +1,8 @@
 // this cannot be interface, it has to be type because this type has no properties
 type TOperator = "+" | "-" | "*" | "/"; // Union type that only accepts the values +, -, *, /
 
-const doMath = (number1: number, number2: number, operator: TOperator = "+") => {
+const operators = ["+", "-", "*", "/"];
+const doMathDefault = (number1: number, number2: number, operator: TOperator = operators[Math.floor(Math.random() * operators.length)] as TOperator) => {
   switch (operator) {
     case "+":
       return number1 + number2;
@@ -16,14 +17,28 @@ const doMath = (number1: number, number2: number, operator: TOperator = "+") => 
   }
 }
 
-const calc1 = doMath(1, 2);
-const calc2 = doMath(1, 2, "-");
-const calc3 = doMath(1, 2, "*");
-const calc4 = doMath(1, 2, "/");
+const doMathOptional = (number1: number, number2: number, operator?: TOperator) => {
+  switch (operator) {
+    case "-":
+      return number1 - number2;
+    case "*":
+      return number1 * number2;
+    case "/":
+      return number1 / number2;
+    case "+":
+    default:
+      return number1 + number2;
+  }
+}
 
-//const calc5 = doMath(1, 2, "p"); // Error: Invalid operator
+const calc1 = doMathDefault(1, 2);
+const calc2 = doMathDefault(1, 2);
+const calc3 = doMathDefault(1, 2);
+const calc4 = doMathDefault(1, 2);
+//const calc5 = doMathDefault(1, 2, "p"); // Error: Invalid operator
 
 console.log(calc1); // 3
 console.log(calc2); // -1
 console.log(calc3); // 2
 console.log(calc4); // 0.5
+
