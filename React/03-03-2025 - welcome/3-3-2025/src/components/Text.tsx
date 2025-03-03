112bee56c1eefbf4671c74b6f67306d9e4b632e5
Yehonatan <yehonatan604@gmail.com>
2025-03-03 21:16:22 +0200

    react app 1

---
+++ React/03-03-2025 - welcome/3-3-2025/src/components/Text.tsx
@@ -0,0 +1,12 @@
+//import "./Text.css";
+import styles from "./Text.module.css";
+
+const Text = () => {
+  return (
+    <div>
+      <h1 className={styles["red-title"]}>Hello My Class Good Evening</h1>
+    </div>
+  );
+};
+
+export default Text;
