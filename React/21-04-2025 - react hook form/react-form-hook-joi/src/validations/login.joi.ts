2ccf2ed928dd2b95503c4eb9a63064d3e9dcc6b7
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-04-21 18:52:41 +0300

    msgs

--- React/21-04-2025 - react hook form/react-form-hook-joi/src/validations/login.joi.ts
+++ React/21-04-2025 - react hook form/react-form-hook-joi/src/validations/login.joi.ts
@@ -1,7 +1,10 @@
 import Joi from 'joi';
 
 export const loginSchema = Joi.object({
-    email: Joi.string().email({ tlds: { allow: false } }).required(),
+    email: Joi.string().email({ tlds: { allow: false } }).required().messages({
+        'string.email': 'Email must be a valid email address.',
+        'string.empty': 'Email is required.',
+    }),
     password: Joi.string().ruleset.regex(/((?=.*\d{1})(?=.*[A-Z]{1})(?=.*[a-z]{1})(?=.*[!@#$%^&*-]{1}).{7,20})/).rule({
         message: "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character, and be between 7 to 20 characters long."
     })
