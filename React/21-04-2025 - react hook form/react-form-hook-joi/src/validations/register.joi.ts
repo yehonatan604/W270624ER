e2014d86ae08d2f3fc6b3e611d15156c749a6051
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-04-24 16:16:25 +0300

    hocs

--- React/21-04-2025 - react hook form/react-form-hook-joi/src/validations/register.joi.ts
+++ React/21-04-2025 - react hook form/react-form-hook-joi/src/validations/register.joi.ts
@@ -11,7 +11,6 @@ export const registerSchema = Joi.object({
 
     phone: Joi.string()
         .ruleset.regex(/0[0-9]{1,2}-?\s?[0-9]{3}\s?[0-9]{4}/)
-
         .rule({ message: 'user "phone" mast be a valid phone number' })
         .required(),
     email: Joi.string()
