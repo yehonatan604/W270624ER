656207dcd2bfd6d8a6a954627a3ffd468005f259
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-04-24 21:03:36 +0300

    finished

--- React/24-04-2025 - hocs/route-guard/src/components/RouteGuard.tsx
+++ React/24-04-2025 - hocs/route-guard/src/components/RouteGuard.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import LoginPage from "../pages/Login.page";
 
 type RouteGuardProps = {
   children: React.ReactNode;
@@ -8,17 +8,9 @@ type RouteGuardProps = {
 const RouteGuard = (props: RouteGuardProps) => {
   const { children, isLoggedIn } = props;
 
-  const content = isLoggedIn ? (
-    <>{children}</>
-  ) : (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-white px-4 py-24 dark:bg-gray-900">
-      <h1 className="text-2xl font-bold">You are not logged in</h1>
-      <p className="mt-4 text-lg">Please log in to access this page.</p>
-      <Link to="/login" className="mt-4 text-blue-500 hover:underline">
-        Login
-      </Link>
-    </div>
-  );
+  // Check if the user is logged in
+  // If not, redirect to the login page
+  const content = isLoggedIn ? <>{children}</> : <LoginPage />;
 
   return content;
 };
