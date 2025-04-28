ebed892feb3c9c282de0d1dde8af5c72bbc1988a
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2025-04-28 20:36:26 +0300

    gitignore

---
+++ React/28-04-2025 - redux/redux-lesson-starter-main/src/store/userSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// type of user
+type Tuser = {
+    _id: string;
+    name: {
+        first: string;
+        middle?: string;
+        last: string;
+    }
+}
+
+// This is the initial state of the user slice
+const initialState = {
+    user: null as Tuser | null,
+}
+
+// this is the user slice of the redux store
+// its created using the createSlice function from redux toolkit
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: { // Actions to update the state
+        login: (state, data) => {
+            state.user = data.payload.user;
+        },
+        logout: (state) => {
+            state.user = null;
+        },
+    },
+});
+
+//export type TUserSliceState = typeof initialState; if we want to go type safety
+export const userActions = userSlice.actions; // this will give us the actions of the slice
+export default userSlice.reducer; // this will give us the slice reducer
